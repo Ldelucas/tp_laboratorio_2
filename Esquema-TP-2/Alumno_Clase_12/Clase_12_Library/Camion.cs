9b6ee2b3902b48f3f85917502e1c46e5903daa97
Lucas <luc4s182@hotmail.com>
2016-10-10 18:45:07 -0300

    Add files via upload

---
+++ Esquema-TP-2/Alumno_Clase_12/Clase_12_Library/Camion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_12_Library
+{
+    public class Camion: Vehiculo
+    {
+        public Camion(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+        }
+        /// <summary>
+        /// Los camiones tienen 8 ruedas
+        /// </summary>
+        public override short CantidadRuedas//no se pueden cambiar los modificadores y no era override, agregue el set
+        {
+            get
+            {
+                return 8;
+            }
+            set { }
+        }
+
+        public override sealed string Mostrar()//no se pueden cambiar los modificadores
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CAMION");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("RUEDAS :"+ this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
